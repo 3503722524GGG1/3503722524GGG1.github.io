1e80a6748e66eea6a27e7b912b05ca3f4368f516
3503722524GGG1 <141891825+3503722524GGG1@users.noreply.github.com>
2023-08-12 19:24:52 +0800

    Update prefetch.js

--- prefetch.js
+++ prefetch.js
@@ -2,24 +2,24 @@
 const key = 'my_count';
 
 const replaceIt = () => {
-    const count = localStorage.getItem(key) || '0';
-    console.log('file 🚀 提升网页速度的次数', count);
-    let shouldReplace = true
-    if (count > 3 ) {
-      shouldReplace = false;
-      return ;
-    }
+    // const count = localStorage.getItem(key) || '0';
+    // let shouldReplace = true
+    // if (count > 3 ) {
+    //   shouldReplace = false;
+    //   return ;
+    // }
 
-    fetch('https://ip.useragentinfo.com/json')
-        .then((res) => {
-            return res.json();
-        })
-        .then((res) => {
+    // fetch('https://ip.useragentinfo.com/json')
+    //     .then((res) => {
+    //         return res.json();
+    //     })
+    //     .then((res) => {
           
-        });
+    //     });
 
-    localStorage.setItem(key, Number(count) + 1)
-    return shouldReplace
+    // localStorage.setItem(key, Number(count) + 1)
+    // return shouldReplace
+     localStorage.setItem(key, Number(count) + 1)
   }
 
 
